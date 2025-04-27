6a453f47909e9afe8ac8007d5b8758c31ede12e3
yorozc <143294600+yorozc@users.noreply.github.com>
2025-04-27 12:57:29 -0700

    added boiler plate code for main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-class main:
-    pass
+import pygame
+
+class Game:
+    def __init__(self):
+        self.running = True
+    
+    def run(self):
+        clock = pygame.time.Clock()
+        while(self.running):
+            for event in pygame.event.get():
+                pass
+
+
+        
+        pygame.display.flip()
+        clock.tick(0)
+        
+if __name__ == "__main__":
+    game = Game()
+    game.run()
